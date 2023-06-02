26d0dc5446ecd4be48da384ab79147dc1e656dd9
Renato Alves <19148962+renatonascalves@users.noreply.github.com>
2023-06-01 22:04:50 -0300

    Make sure we have a registry before using it

--- src/Schema/Types/BlockTypes.php
+++ src/Schema/Types/BlockTypes.php
@@ -262,6 +262,10 @@ class BlockTypes {
 
 			$registry = Registry::get_registry();
 
+			if ( empty( $registry ) || ! is_array( $registry ) ) {
+				return;
+			}
+
 			foreach ( $registry as $block_name => $block_type ) {
 				$type_names[] = self::register_block_type( $block_type, $type_registry );
 			}
